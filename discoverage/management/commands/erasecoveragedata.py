06fe5ecc47951a373db89ae666d01adc3fed2211
Omer Katz <omer.drow@gmail.com>
2013-01-26 21:12:57 +0300

    Improved the erasecoveragedata management command. The command now verifies that the coverage data file exists and the coverage data filename defaults to .coverage

--- discoverage/management/commands/erasecoveragedata.py
+++ discoverage/management/commands/erasecoveragedata.py
@@ -1,8 +1,19 @@
-from coverage import coverage
-from django.core.management import BaseCommand
+from optparse import make_option
+import os
+from django.core.management import BaseCommand, CommandError
+from discoverage.utils import CoverageHandler
 
 class Command(BaseCommand):
-    def handle(self, filename, **options):
-        cov = coverage(data_file=filename)
+    option_list = BaseCommand.option_list + (
+        make_option('--filename',
+            action='store', dest='filename', default='.coverage',
+            help='Specifies the coverage data file to be erased. Defaults to .coverage'),
+        )
+
+    def handle(self, filename='.coverage', **options):
+        cov = CoverageHandler(data_file=filename)
+
+        if not os.path.exists(filename):
+            raise CommandError("'%s' coverage data file does not exist." % filename)
 
         cov.erase()
