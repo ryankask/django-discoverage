119f01f72fa442f41115ade563f571d92e85da7e
Ryan Kaskel <dev@ryankaskel.com>
2013-01-29 07:39:27 +0000

    Remove erase coverage data command (use coverage erase instead).

--- discoverage/management/commands/erasecoveragedata.py
+++
@@ -1,9 +0,0 @@
-from coverage import coverage
-
-from django.core.management import BaseCommand
-
-
-class Command(BaseCommand):
-    def handle(self, filename, **options):
-        cov = coverage(data_file=filename)
-        cov.erase()
