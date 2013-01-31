ed93f042f527f1fa511e3072de0c2e2e1028f3a4
Ryan Kaskel <dev@ryankaskel.com>
2013-01-31 08:03:54 +0000

    Add test case.

--- test_project/apps/accounts/utils.py
+++ test_project/apps/accounts/utils.py
@@ -1,2 +1,5 @@
 def multiply_numbers(x, y):
     return x * y
+
+def concat(r, s):
+    return r + s
--- test_project/tests/accounts/test_utils.py
+++ test_project/tests/accounts/test_utils.py
@@ -1,8 +1,11 @@
 from django.utils import unittest
 
-from accounts.utils import multiply_numbers
+from accounts.utils import multiply_numbers, concat
 
 
 class UtilsTests(unittest.TestCase):
     def test_multiply_numbers(self):
         self.assertEqual(multiply_numbers(2, 3), 6)
+
+    def test_concat(self):
+        self.assertEqual(concat('re', 'lease'), 'release')
